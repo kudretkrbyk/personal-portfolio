9643e4b4f7cf6cdb3f612cecf91e5b5f3628e4a3
kudretkrbyk <kudretkrbyk@gmail.com>
2025-05-16 13:35:14 +0300

    added production.env

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { store } from "./store.js";
+import { Provider } from "react-redux";
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
+  </StrictMode>
+);
